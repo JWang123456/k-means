36ee529b35925b2b973b3ad6bdd5619d97e5d39f
JWang123456 <wanggaojie913@yahoo.com>
2018-11-28 15:26:35 -0600

    M

--- kmeans_lloyd.py
+++ kmeans_lloyd.py
@@ -48,17 +48,18 @@ def initCentroids(dataSet, k):
 
 def kmeansIter(dataSet, k, r):
 	
-	numSamples = dataSet.shape[0]  #读取矩阵dataSet的第一维度的长度,即获得有多少个样本数据
-    # first column stores which cluster this sample belongs to,  
-    # second column stores the error between this sample and its centroid  
-	clusterAssment = mat(zeros((numSamples, 2)))  #得到一个N*2的零矩阵
-	clusterChanged = True  
-  
-    ## step 1: init centroids  
-	centroids = initCentroids(dataSet, k)  #在样本集中随机选取k个样本点作为初始质心
-
 	num = 1
 	while num <= r:
+		numSamples = dataSet.shape[0]  #读取矩阵dataSet的第一维度的长度,即获得有多少个样本数据
+		# first column stores which cluster this sample belongs to,  
+		# second column stores the error between this sample and its centroid  
+		clusterAssment = mat(zeros((numSamples, 2)))  #得到一个N*2的零矩阵
+		clusterChanged = True  
+	
+		## step 1: init centroids  
+		centroids = initCentroids(dataSet, k)  #在样本集中随机选取k个样本点作为初始质心
+		# print('centroids', centroids)
+		
 		while clusterChanged:  
 			clusterChanged = False  
 			## for each sample  
@@ -80,6 +81,7 @@ def kmeansIter(dataSet, k, r):
 				if clusterAssment[i, 0] != minIndex:  
 					clusterChanged = True  
 					clusterAssment[i, :] = minIndex, minDist**2  #两个**表示的是minDist的平方
+			
 	
 			## step 4: update centroids  
 			for j in range(k):  
@@ -88,11 +90,17 @@ def kmeansIter(dataSet, k, r):
 				pointsInCluster = dataSet[nonzero(clusterAssment[:, 0].A == j)[0]] #将dataSet矩阵中相对应的样本提取出来 
 				
 				centroids[j, :] = mean(pointsInCluster, axis = 0)  #计算标注为j的所有样本的平均值
-	
-		print (f'Iteration {num} complete!')  
-		# return centroids, clusterAssment  
+			
+		print(f'Iteration {num} complete!')  
+		# return centroids, clusterAssment 
+		err = 0
+		
+		for i in clusterAssment[:, 1]:
+			err += i		
+		print(err) 
+		
 		num += 1
-
+		
 	return reshape(clusterAssment[:, 0], (1, numSamples)).astype(int)
 
 # print(res)
