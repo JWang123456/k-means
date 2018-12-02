6a31dd59ca248fbae3205cf91276542911cb3bc0
JWang123456 <wanggaojie913@yahoo.com>
2018-12-01 18:44:31 -0600

    M

--- kmeanspp.py
+++ kmeanspp.py
@@ -1,189 +1,166 @@
-  
+
+import sys
 from numpy import *  
 import time  
-import matplotlib.pyplot as plt  
-import sys
-  
-import random
-import numpy as np
+import matplotlib.pyplot as plt
 
+# if len(sys.argv) != 5:
+#     print('usage: ', sys.argv[0], 'data_file k r output_file')
+#     sys.exit()
 
-DEBUG = True
+#Read inputs.
+
+# one row one data point, columns are dimentions
+dataSet = genfromtxt(sys.argv[1], delimiter=',', autostrip=True) # strip spaces
+
+# separate or clustering data int to k groups, clasters.
+# what ever you call it
+k = int(sys.argv[2])
+
+# r iterations: run the algorithm r times.
+# at each time the algorithm should converge
+r = int(sys.argv[3])
+
+# calculate Euclidean distance  
+def euclDistance(vector1, vector2):  
+	return sqrt(sum(power(vector2 - vector1, 2)))  #求这两个矩阵的距离，vector1、2均为矩阵
+
+DEBUG = False
 
 def dist(v1, v2):
-    return np.linalg.norm(v1 - v2)
+    return linalg.norm(v1 - v2)
 
-def is_c_closet_to_x(x, c, all_clusters):
-    return all(dist(x, other_cluster) >= dist(x, c) for other_cluster in all_clusters)
 
-class KMeans:
-
-    def get_params(self):
-        return {"n_clusters": self.n_clusters, "seeding_algorithm": self.seeds_algo,
-                "max_iterations": self.max_iterations, "t": self.t, "l":self.l}
-
-    def set_params(self, n_clusters=2, seeds_algo='scalable-kmeans++', update_algo=None, max_iterations=None, t=2, l=5):
-        self.n_clusters = n_clusters
-        self.seeds_algo = seeds_algo
-        self.max_iterations = max_iterations
-        self.t = t
-        self.l = l
-
-    def fit(self, X):
+def kmeanspp(X, k, W=None):
+    if W == None:
+        W = [1] * len(X)
+    centroids_indices = [random.choice(len(X), 1, p=[w / sum(W) for w in W])[0]]
+    if DEBUG:
+        print('First centroid index drawn at random:')
+        print(centroids_indices)
+    for i in range(k - 1):
         if DEBUG:
-            print('entering fit')
-
-        self.import sysinit_seeds(X)
-
-        if self.max_iterations == None:
-            self.max_iterations = sys.maxsize
-        
-        itteration = 0
-        cluster_updated = True
-        
-        while itteration < self.max_iterations and cluster_updated:
-            if DEBUG:
-                print('iteration ' + str(itteration))
-            
-            centroids_to_x_dict = self.group_x_by_clusters(X)
-            
-            if DEBUG:
-                print('closest centroid to sample list')
-                print(centroids_to_x_dict.values())
-            
-            old_centroids = self.centroids
-            
-            self.centroids = [np.mean(np.array([X[x_idx] for x_idx in centroids_to_x_dict[c_idx]]), axis=0) for c_idx in
-                              range(len(self.centroids))]
-            
-            if DEBUG:
-                print('new centroids')
-                print(self.centroids)
-            
-            cluster_updated = any(
-                not np.array_equal(self.centroids[c_idx], old_centroids[c_idx]) for c_idx in range(len(self.centroids)))
-            # print(self.centroids)
-
-        self.total_itterations = itteration
-        
-        return self.predict(X)
-
-    def get_cluster_label_for_x(self, X, centroids_to_x_dict):
-        results = []
-        c_idx_to_new_cluster_map = {}
-        for x_idx, x in enumerate(X):
-            for c_idx, x_indices_belong_to_c in centroids_to_x_dict.items():
-                if x_idx in x_indices_belong_to_c:
-                    if c_idx not in c_idx_to_new_cluster_map:
-                        c_idx_to_new_cluster_map[c_idx] = len(c_idx_to_new_cluster_map) + 1
-                    results.append(c_idx_to_new_cluster_map[c_idx])
-                    break
-        return results
-
-    def group_x_by_clusters(self, X):
-        centroids_to_x_dict = {c_idx: set() for c_idx in range(len(self.centroids))}
-        for x_idx, x in enumerate(X):
-            dist_to_all_clusters = [dist(x, c) for c in self.centroids]
-            min_dist = min(dist_to_all_clusters)
-            closest_centroid_idx = dist_to_all_clusters.index(min_dist)
-            centroids_to_x_dict[closest_centroid_idx].add(x_idx)
-        return centroids_to_x_dict
-
-    def init_seeds(self, X):
-        start = time.time()
-        if self.seeds_algo == 'forgy':
-            self.centroids = random.sample(X, self.n_clusters)
-        if self.seeds_algo == 'random':
-            self.init_seeds_random(X)
-        if self.seeds_algo == 'kmeans++':
-            self.init_seeds_kmeanspp(X)
-        if self.seeds_algo == 'scalable-kmeans++':
-            self.init_seeds_scalable_kmeanspp(X)
-        self.seeds_init_duration = time.time() - start
-
-    def init_seeds_random(self, X):
-        assignments = []
-        for i in range(self.n_clusters):
-            assignments.append([])
-        for i in range(len(X)):
-            randint = random.randint(0, self.n_clusters - 1)
-            assignments[randint].append(X[i])
-        if any(len(assignments[n]) == 0 for n in range(len(assignments))):
-            print('recursive')
-            self.init_seeds(X)
-        self.centroids = []
-        for n in range(self.n_clusters):
-            self.centroids.append(np.mean(np.array(assignments[n])))
-
-    def init_seeds_kmeanspp(self, X, W=None):
-        if W == None:
-            W = [1] * len(X)
-        centroids_indices = [np.random.choice(len(X), 1, p=[w / sum(W) for w in W])[0]]
+            print('iteration i = ' + str(i + 1))
+        x_dist_to_closest_centroids = [W[i] * min(dist(X[i], X[c]) ** 2 for c in centroids_indices) for i in
+                                        range(len(X))]
+        x_probs = [d / sum(x_dist_to_closest_centroids) for d in x_dist_to_closest_centroids]
         if DEBUG:
-            print('First centroid index drawn at random:')
-            print(centroids_indices)
-        for k in range(self.n_clusters - 1):
+            print('Distribution at iteration:')
+            print(x_probs)
+        try:
+            centroids_indices.append(random.choice(len(X), 1, p=x_probs)[0])
             if DEBUG:
-                print('iteration k = ' + str(k + 1))
-            x_dist_to_closest_centroids = [W[i] * min(dist(X[i], X[c]) ** 2 for c in centroids_indices) for i in
-                                           range(len(X))]
-            x_probs = [d / sum(x_dist_to_closest_centroids) for d in x_dist_to_closest_centroids]
-            if DEBUG:
-                print('Distribution at iteration:')
-                print(x_probs)
-            try:
-                centroids_indices.append(np.random.choice(len(X), 1, p=x_probs)[0])
-                if DEBUG:
-                    print('centorids indices at the end of iteration')
-                    print(x_probs)
-            except:
-                print(x_probs)
-        self.centroids = list([X[i] for i in centroids_indices])
+                print('centorids indices at the end of iteration')
+                print(centroids_indices)
+        except:
+            print(x_probs)
+            
+    numSamples, dim = X.shape   #矩阵的行数、列数 
+    res = zeros((k, dim))
 
-    def predict(self, X):
-        centroids_to_x_dict = self.group_x_by_clusters(X)
-        results = self.get_cluster_label_for_x(X, centroids_to_x_dict)
-        return results
+    for i in range(k):
+        res[i, :] = X[centroids_indices[i], :]
+    
+    return res
 
-    def init_seeds_scalable_kmeanspp(self, X):
-        B_indices = self.overseeding_scalable_kmeanspp(X)
-        B = [X[i] for i in B_indices]
-        W = self.seeding_scalable_kmeanspp(B, X)
-        self.init_seeds_kmeanspp(X=B, W=W)
+# def initCentroids(dataSet, k, centroidsIndex):  
+# 	numSamples, dim = dataSet.shape   #矩阵的行数、列数 
+# 	centroids = zeros((k, dim))  		#感觉要不要你都可以
+# 	for i in centroidsIndex:  
+# 		centroids[i, :] = dataSet[i, :]  
+# 	return centroids
 
-    def overseeding_scalable_kmeanspp(self, X):
 
-        c_indices = [random.randint(0, len(X) - 1)]
-        if DEBUG:
-            print("entering overseeding")
-            print('random first index in B')
-            print(c_indices)
-        for i in range(self.t):
-            c_tag_indices = []
-            x_dist_to_closest_centroids = [min(dist(X[x_idx], X[c]) ** 2 for c in c_indices) for x_idx in range(len(X))]
-            x_probs = [min(1, self.l * d / sum(x_dist_to_closest_centroids)) for d in x_dist_to_closest_centroids]
-            if DEBUG:
-                print("probabilites:")
-                print(x_probs)
 
-            for x_idx in range(len(X)):
-                if x_probs[x_idx] >= random.uniform(0, 1):
-                    c_tag_indices.append(x_idx)
-            c_indices += c_tag_indices
-            if DEBUG:
-                print('Samples indices joined to B:')
-                print(c_indices)
-        return c_indices
+def kmeansIter(dataSet, k, r):
+	
+	num = 1
+	while num <= r:
+		numSamples = dataSet.shape[0]  #读取矩阵dataSet的第一维度的长度,即获得有多少个样本数据
+		# first column stores which cluster this sample belongs to,  
+		# second column stores the error between this sample and its centroid  
+		clusterAssment = mat(zeros((numSamples, 2)))  #得到一个N*2的零矩阵
+		clusterChanged = True  
+	
+		## step 1: init centroids  
+		centroids = kmeanspp(dataSet, k, W=None)  #在样本集中随机选取k个样本点作为初始质心
+		# print('centroids', centroids)
+		
+		while clusterChanged:  
+			clusterChanged = False  
+			## for each sample  
+			for i in range(numSamples):  #range
+				minDist  = 100000.0  
+				minIndex = 0  
+				## for each centroid  
+				## step 2: find the centroid who is closest  
+				#计算每个样本点与质点之间的距离，将其归内到距离最小的那一簇
+				for j in range(k):  
+					distance = euclDistance(centroids[j, :], dataSet[i, :])  
+					if distance < minDist:  
+						minDist  = distance  
+						minIndex = j  
+				
+				## step 3: update its cluster 
+				#k个簇里面与第i个样本距离最小的的标号和距离保存在clusterAssment中
+				#若所有的样本不在变化，则退出while循环
+				if clusterAssment[i, 0] != minIndex:  
+					clusterChanged = True  
+					clusterAssment[i, :] = minIndex, minDist**2  #两个**表示的是minDist的平方
+			
+	
+			## step 4: update centroids  
+			for j in range(k):  
+				#clusterAssment[:,0].A==j是找出矩阵clusterAssment中第一列元素中等于j的行的下标，返回的是一个以array的列表，第一个array为等于j的下标
+				
+				pointsInCluster = dataSet[nonzero(clusterAssment[:, 0].A == j)[0]] #将dataSet矩阵中相对应的样本提取出来 
+				
+				centroids[j, :] = mean(pointsInCluster, axis = 0)  #计算标注为j的所有样本的平均值
+			
+		print(f'Iteration {num} complete!')  
+		# return centroids, clusterAssment 
+		err = 0
+		
+		for i in clusterAssment[:, 1]:
+			err += i		
+		print(err) 
+		
+		num += 1
+		
+	return reshape(clusterAssment[:, 0], (1, numSamples)).astype(int)
 
-    def seeding_scalable_kmeanspp(self, B, X):
-        calculated_x_indices = set()
-        W = []
-        for b in B:
-            points_closest_to_c = [x_idx for x_idx in range(len(X)) if is_c_closet_to_x(x=X[x_idx], c=b,
-                                                                                        all_clusters=B) and x_idx not in calculated_x_indices]
-            W.append(len(points_closest_to_c))
-            calculated_x_indices.update(points_closest_to_c)
-        if DEBUG:
-            print('Weights of B:')
-            print(W)
-        return W
+# print(res)
+#Save output in a comma separated file. 
+#File name should pass from command line.
+
+res = kmeansIter(dataSet, k, r)
+# labels = array([1, 2, 3])
+savetxt(sys.argv[4], res, delimiter=',')
+
+
+# show your cluster only available with 2-D data 
+#centroids为k个类别，其中保存着每个类别的质心
+#clusterAssment为样本的标记，第一列为此样本的类别号，第二列为到此类别质心的距离 
+def showCluster(dataSet, k, centroids, clusterAssment):  
+	numSamples, dim = dataSet.shape  
+	if dim != 2:  
+		print ("Sorry! I can not draw because the dimension of your data is not 2!")  
+		return 1  
+  
+	mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']  
+	if k > len(mark):  
+		print ("Sorry! Your k is too large! please contact wojiushimogui")  
+		return 1 
+     
+  
+    # draw all samples  
+	for i in range(numSamples):  
+		markIndex = int(clusterAssment[i, 0])  #为样本指定颜色
+		plt.plot(dataSet[i, 0], dataSet[i, 1], mark[markIndex])  
+  
+	mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']  
+    # draw the centroids  
+	for i in range(k):  
+		plt.plot(centroids[i, 0], centroids[i, 1], mark[i], markersize = 12)  
+  
+	plt.show() 
